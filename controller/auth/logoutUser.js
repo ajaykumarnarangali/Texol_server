8bf0add73cd1a41670f5275faf90a9a538ef0db1
ajaykumarnarangali <www.ajayanarangali@gmail.com>
2025-07-19 10:47:44 +0530

    fixed cookie bug

--- controller/auth/logoutUser.js
+++ controller/auth/logoutUser.js
@@ -3,6 +3,9 @@ exports.logoutUser = async (req, res, next) => {
     try {
         res.clearCookie('refresh_token', {
             path: '/',
+            httpOnly: true,
+            secure: true, 
+            sameSite: 'None',
         }).clearCookie('refresh_token').status(200).json({
             message: "User details fetched successfully",
             success: true,
